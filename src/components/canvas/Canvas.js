af30e1a7b7db9634eb991111a46c11beb9cdb58e
LouiseHsu <louiseh0313@gmail.com>
2019-11-20 20:56:43 -0800

    canvas wip

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Pixel from '../pixel/Pixel';
+import './Canvas.css';
 
-const canvasSize = Object.freeze({
+const canvasSizeEnum = Object.freeze({
     TEST: {width: 2, height: 1},
     SMALL: {width: 50, height: 50},
     MEDIUM: {width: 100, height: 100},
@@ -9,9 +10,42 @@ const canvasSize = Object.freeze({
 });
 const Canvas = props => {
 
+    const [canvasSize, setCanvasSize] = useState('Medium');
     const [canvasPixels, setCanvasPixels] = useState([]);
 
-    return (<div className = "Canvas"><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/></div>);
+    const handleSize = (size) => {
+        setCanvasSize(size);
+        let pixelNum;
+
+        switch (canvasSize) {
+            case 'Large':
+                pixelNum = canvasSizeEnum.LARGE.width * canvasSizeEnum.LARGE.height;
+                break;
+            case 'Medium':
+                pixelNum = canvasSizeEnum.MEDIUM.width * canvasSizeEnum.MEDIUM.height;
+                break;
+            default:
+                pixelNum = 1;
+        }
+
+        let pixels = [];
+        for (let i = 0; i < pixelNum; i++) {
+            pixels.push(<Pixel key={i} />)
+        }
+
+        setCanvasPixels(pixels);
+    };
+
+    useEffect(() => {
+        handleSize(canvasSize);
+    }, [props.size]);
+
+    return (
+        <div className="Canvas">
+            {canvasPixels}
+        </div>);
 };
+
+
 
 export default Canvas;
