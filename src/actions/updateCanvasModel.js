c0da01f5849d4ff068dbb412472c31cb70e6d5a2
LouiseHsu <36148254+LouiseHsu@users.noreply.github.com>
2019-12-29 03:21:06 -0800

    starting radius algo

--- src/actions/updateCanvasModel.js
+++ src/actions/updateCanvasModel.js
@@ -1,10 +1,27 @@
 import {store} from './../store';
 
 export const updateCanvasModel = (clickedPixel) => {
-    let updatedPixel = [clickedPixel];
+    let updatedPixel = [];
+    let brushState = store().getState().brushState;
+
+    let startY = clickedPixel.y - brushState.radius;
+    let endY = clickedPixel.y + brushState.radius;
+    let startX = clickedPixel.x - brushState.radius;
+    let endX = clickedPixel.x + brushState.radius;
+
+    for (let x = startX; x < endX; x++) {
+        for (let y = startY; y < endY; y++) {
+            updatedPixel.push(
+                {
+                    x,
+                    y
+                }
+            )
+        }
+    }
     return {
         type: 'UPDATE_CANVAS_MODEL',
-        updatedPixels : updatedPixel,
-        currBrushColour: store().getState().brushState.colour
+        updatedPixels: updatedPixel,
+        currBrushColour: brushState.colour
     }
 };
