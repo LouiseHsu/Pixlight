31ca37726760ad690775d610ff4ac6477e2c8183
LouiseHsu <36148254+LouiseHsu@users.noreply.github.com>
2020-01-08 01:40:23 -0800

    can change brush size

--- src/components/toolbar/brushRadius/BrushRadius.js
+++ src/components/toolbar/brushRadius/BrushRadius.js
@@ -1,19 +1,18 @@
 import {useDispatch} from "react-redux";
-import React, {useState} from "react";
+import React from "react";
 import {changeBrushRadius} from "../../../actions/changeBrushRadius";
 
 const BrushRadius = props => {
-    const [inputRadius, setInputRadius] = useState(1);
 
-    const handleChange = (val) => {
-        setInputRadius(val);
-        dispatch(changeBrushRadius(val));
+    let handleChange = (val) => {
+        // dispatch(changeBrushRadius(3));
+        dispatch(changeBrushRadius(parseInt(val)));
     };
 
     let dispatch = useDispatch();
     return <>
         <button onClick={() => dispatch(changeBrushRadius(3))}>Change Brush Radius to 3</button>
-        <input type = "number" min = "1" onChange={() => handleChange(3)}/>
+        <input type = "number" min = "1" onChange={(e) => handleChange(e.target.value)}/>
     </>
 };
 
